8d73a3fa1b8a1598bb91bee1f829e2cc049d6e2c
Gaurav Patil <Gaurav Patil>
2024-11-15 16:53:07 +0530

    assets folder

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,10 +7,10 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <section className="bg-brand p-10">
         <div>
           <Image
-            src="/favicon.ico"
+            src="/assets/icons/logo-full.svg"
             alt="Logo"
-            width={16}
-            height={16}
+            width={224}
+            height={82}
             className="h-auto"
           />
           <div className="space-y-5 text-white">
@@ -19,6 +19,13 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
               This is the place where you can store all your documents
             </p>
           </div>
+          <Image
+            src="/assets/images/files.png"
+            alt="Files"
+            width={342}
+            height={342}
+            className="h-auto"
+          />
         </div>
       </section>
       {children}
