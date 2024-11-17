17c507dfa9923f97f1ffe44569486ed68290272f
Gaurav Patil <gauravpatil@thehosteller.com>
2024-11-17 23:15:11 +0530

    signup using appwrite

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen">
-      <section className="bg-brand p-10">
-        <div>
+      <section className="bg-brand h-screen p-10 hidden w-1/2 lg:flex justify-center items-center xl:w-2/5">
+        <div className="max-h-[800px] max-w-[400px] space-y-12">
           <Image
             src="/assets/icons/logo-full.svg"
             alt="Logo"
@@ -13,10 +13,11 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             height={82}
             className="h-auto"
           />
-          <div className="space-y-5 text-white">
+          <div className="space-y-10 text-white">
             <h1 className="h1">Manage your files the best way</h1>
             <p className="body-1">
-              This is the place where you can store all your documents
+              Awesome, we've created the perfect place for you to store all your
+              documents.
             </p>
           </div>
           <Image
@@ -24,11 +25,22 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             alt="Files"
             width={342}
             height={342}
-            className="h-auto"
+            className="transition-all hover:scale-105 hover:rotate-3"
           />
         </div>
       </section>
-      {children}
+      <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
+        <div className="mb-10 lg:hidden">
+          <Image
+            src="/assets/icons/logo-full-brand.svg"
+            alt="Logo"
+            width={224}
+            height={82}
+            className="h-auto"
+          />
+        </div>
+        {children}
+      </section>
     </div>
   );
 };
---
+++ components/AuthForm.tsx
@@ -0,0 +1,151 @@
+"use client";
+
+import { useState } from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import Link from "next/link";
+import { createAccount } from "@/lib/actions/user.actions";
+
+type FormType = "sign-in" | "sign-up";
+
+const authFormSchema = (formType: FormType) => {
+  return z.object({
+    fullName:
+      formType === "sign-up"
+        ? z.string().min(2).max(50)
+        : z.string().optional(),
+    email: z.string().email(),
+  });
+};
+
+const AuthForm = ({ type }: { type: "sign-in" | "sign-up" }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [accountId, setAccountId] = useState(null);
+
+  const formSchema = authFormSchema(type);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fullName: "",
+      email: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    setIsLoading(true);
+
+    const user = await createAccount({
+      fullName: values.fullName || "",
+      email: values.email,
+    });
+
+    setAccountId(user.accountId);
+
+    setIsLoading(false);
+
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values, "values");
+  };
+
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
+          <h1 className="form-title">
+            {type === "sign-in" ? "Login" : "Create Account"}
+          </h1>
+          {type === "sign-up" && (
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="shad-form-item">
+                    <FormLabel className="shad-form-label">Full Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter your full name"
+                        {...field}
+                        className="shad-input"
+                      />
+                    </FormControl>
+                  </div>
+                  <FormMessage className="shad-form-message" />
+                </FormItem>
+              )}
+            />
+          )}
+
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <div className="shad-form-item">
+                  <FormLabel className="shad-form-label">Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter your email"
+                      {...field}
+                      className="shad-input"
+                    />
+                  </FormControl>
+                </div>
+                <FormMessage className="shad-form-message" />
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit"
+            className="form-submit-button"
+            disabled={isLoading}
+          >
+            {type === "sign-in" ? "Login" : "Create Account"}
+            {isLoading && (
+              <Image
+                src="/assets/icons/loader.svg"
+                width={24}
+                height={24}
+                alt="loader"
+                className="animate-spin ml-2"
+              />
+            )}
+          </Button>
+          {errorMessage && <p className="error-message">{errorMessage}</p>}
+          <div className="body-2 flex  justify-center">
+            <p className="text-ligt-100">
+              {type === "sign-in"
+                ? "Don't have an account?"
+                : "Already have an account?"}
+            </p>
+            <Link
+              href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+              className="ml-1 font-medium text-brand"
+            >
+              {type === "sign-in" ? "Create Account" : "Login"}
+            </Link>
+          </div>
+        </form>
+      </Form>
+      {/* OTP Verification */}
+    </>
+  );
+};
+
+export default AuthForm;
